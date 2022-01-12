0e9ced19dc8883e956058692670d0c773b2d2a4d
AsifHasanChowdhury <72686397+AsifHasanChowdhury@users.noreply.github.com>
2022-01-12 19:32:21 +0600

    Add files via upload

---
+++ test/java/com/practice/demo/springsocialmedia/service/AuthServiceTest.java
@@ -0,0 +1,34 @@
+package com.practice.demo.springsocialmedia.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AuthServiceTest {
+
+    @Test
+    void signup() {
+
+    }
+
+    @Test
+    void getCurrentUser() {
+    }
+
+    @Test
+    void verifyAccount() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void refreshToken() {
+    }
+
+    @Test
+    void isLoggedIn() {
+    }
+}
